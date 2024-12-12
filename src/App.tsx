61892363107d3a68c61b9c005a26696e3bf00c3b
Levente <168279599+tothlevente@users.noreply.github.com>
2024-12-12 09:12:22 +0000

    Develop App.tsx component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "@/components/theme-provider";
+import { useEffect, useState } from "react";
 
 import Repositories from "./components/contents/Repositories";
 import Introducing from "./components/contents/Introducing";
@@ -6,15 +7,63 @@ import Footer from "./components/contents/Footer";
 import Header from "./components/contents/Header";
 
 export default function App() {
-  return (
-    <ThemeProvider
-      defaultTheme="light"
-      storageKey="vite-ui-theme"
-    >
-      <Header />
-      <Introducing />
-      <Repositories />
-      <Footer />
-    </ThemeProvider>
-  );
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [error, setError] = useState<string | null>();
+
+  const [introducing, setIntroducing] = useState([]);
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/tothlevente", {
+      headers: {
+        Authorization: `token ${import.meta.env.VITE_TOKEN}`,
+      },
+    })
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setIntroducing(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+
+    fetch("https://api.github.com/users/tothlevente/repos", {
+      headers: {
+        Authorization: `token ${import.meta.env.VITE_TOKEN}`,
+      },
+    })
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setRepositories(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div className="text-lg font-semibold ">{error}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ThemeProvider
+        defaultTheme="light"
+        storageKey="vite-ui-theme"
+      >
+        <Header />
+        <Introducing />
+        <Repositories />
+        <Footer />
+      </ThemeProvider>
+    );
+  }
 }
