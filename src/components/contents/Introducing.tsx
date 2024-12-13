b1067f9fc02fd2119e0cdb223299fe9b96977fc0
Levente <168279599+tothlevente@users.noreply.github.com>
2024-12-13 10:37:20 +0000

    Develop Introducing.tsx

--- src/components/contents/Introducing.tsx
+++ src/components/contents/Introducing.tsx
@@ -1,3 +1,37 @@
-export default function Introducing() {
-  return <div>Introducing</div>;
+import IntroducingProps from "@/interfaces/IntroducingProps";
+
+export default function Introducing({
+  bio,
+  avatar_url,
+}: IntroducingProps) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        width: "700px",
+        margin: "30px",
+        justifySelf: "center",
+      }}
+    >
+      <img
+        src={avatar_url}
+        alt="Levente"
+        width="250px"
+        height="250px"
+        style={{ borderRadius: "50%" }}
+      />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          margin: "20px",
+        }}
+      >
+        <p style={{ fontWeight: "bold" }}>Hi! 👋</p>
+        <p>My name is Levente.</p>
+        <p>{bio}</p>
+      </div>
+    </div>
+  );
 }
